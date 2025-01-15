3d72981a0b248ceca167e8344c8abd29f3e934a3
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-15 11:16:41 +0000

    created my first greeting, exported and imported, then displayed in main.jsx.

---
+++ my-first-react-project/src/greeting.jsx
@@ -0,0 +1,5 @@
+function MyFirstGreeting() {
+    return <h1>Hello, world! This is my first greeting using React</h1>
+}
+
+export default MyFirstGreeting;
--- my-first-react-project/src/main.jsx
+++ my-first-react-project/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import MyFirstGreeting from './greeting.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <MyFirstGreeting />
     <App />
   </StrictMode>,
 )
