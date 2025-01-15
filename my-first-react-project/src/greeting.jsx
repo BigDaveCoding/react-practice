5acaf6b9d34fb8156ef3745121ed0d0ba7d6d45c
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-15 14:08:55 +0000

    odin project object constructor but I made one in React to practce react knowledge

--- my-first-react-project/src/greeting.jsx
+++ my-first-react-project/src/greeting.jsx
@@ -6,3 +6,14 @@ export function MyFavouriteFoods({foods}) {
     const food_list = foods.map((food, index) => <li key={`food_${index}`}>{food}</li>)
     return food_list
 }
+
+export function BookConstructor(title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+
+    this.bookInfo = function() {
+        return <p>{this.title} by {this.author}, {this.pages} pages, {this.read ? 'read' : 'not read yet'}</p>
+    }
+}
--- my-first-react-project/src/main.jsx
+++ my-first-react-project/src/main.jsx
@@ -2,15 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { MyFirstGreeting, MyFavouriteFoods } from './greeting.jsx'
+import { MyFirstGreeting, MyFavouriteFoods, BookConstructor } from './greeting.jsx'
 
 
 const fav_foods = ['Pizza', 'Pasta', 'Burger', 'Sandwich', 'Hotdog']
+const book_01 = new BookConstructor('The Hobbit', 'J.R.R. Tolkien', 295, true)
+const book_02 = new BookConstructor('The Lord of the Rings', 'J.R.R. Tolkien', 1178, false)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MyFirstGreeting />
     <MyFavouriteFoods foods={fav_foods} />
+    <div>
+      {book_01.bookInfo()}
+      {book_02.bookInfo()}
+    </div>
     <App />
   </StrictMode>,
 )
