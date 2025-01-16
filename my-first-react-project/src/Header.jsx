548bcc83330abcb83a7509a200caaff18c876efc
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-16 10:39:55 +0000

    removing original content and css. creating a header component

---
+++ my-first-react-project/src/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header{
+
+    return (
+        <header>
+            <h1>My First React Project</h1>
+        </header>
+    )
+
+} 
+
+export default Header;
--- my-first-react-project/src/main.jsx
+++ my-first-react-project/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { MyFirstGreeting, MyFavouriteFoods, BookConstructor } from './greeting.jsx'
+// import { MyFirstGreeting, MyFavouriteFoods, BookConstructor } from './greeting.jsx'
 
-import { CreateBook, BookForm, My_books, DisplayBooks } from './library.jsx'
+// import { CreateBook, BookForm, My_books, DisplayBooks } from './library.jsx'
 
 
 
@@ -13,9 +13,8 @@ const fav_foods = ['Pizza', 'Pasta', 'Burger', 'Sandwich', 'Hotdog']
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MyFirstGreeting />
-    <DisplayBooks />
-    {/* <BookForm /> */}
+    {/* <MyFirstGreeting />
+    <DisplayBooks /> */}
     <App />
   </StrictMode>,
 )
