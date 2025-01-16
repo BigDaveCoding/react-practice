12452017bf115d780432383dd0d5647c6c73f6bc
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-16 13:27:14 +0000

    created a normalize css file and also imported script tags for gsap animations in index html so they are availabe globally

--- my-first-react-project/src/main.jsx
+++ my-first-react-project/src/main.jsx
@@ -2,19 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import { MyFirstGreeting, MyFavouriteFoods, BookConstructor } from './greeting.jsx'
-
-// import { CreateBook, BookForm, My_books, DisplayBooks } from './library.jsx'
-
-
-
-
-const fav_foods = ['Pizza', 'Pasta', 'Burger', 'Sandwich', 'Hotdog']
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <MyFirstGreeting />
-    <DisplayBooks /> */}
     <App />
   </StrictMode>,
 )
