e00276475bf5173323ad5bc33bf654338694f7d4
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-16 11:11:17 +0000

    working on my header.

--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { DisplayBooks } from './library.jsx'
+import Header from './Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,6 +30,7 @@ function App() {
           Click on the Vite and React logos to learn more
         </p>
       </div> */}
+      <Header />
       <DisplayBooks />
 
     </>
--- my-first-react-project/src/Header.jsx
+++ my-first-react-project/src/Header.jsx
@@ -1,12 +1,37 @@
 
-function Header{
+function Header() {
+
+    const name = 'David Smith'
+    const role = 'Software Engineer'
+    const date = new Date().getUTCDate() + '/' + new Date().getUTCMonth() + '/' + new Date().getUTCFullYear()
+    const now = new Date();
+    const hours = now.getUTCHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const time = `${hours}:${minutes}`;
+    
 
     return (
         <header>
-            <h1>My First React Project</h1>
+            <div className="header_grid">
+                <div className="date_time header_grid_item">
+                    <p>{date}</p>
+                    <p>{time}</p>
+                </div>
+                <div className="page_title header_grid_item">
+                    <h1>{name.toUpperCase()}</h1>
+                    <p>{role.toUpperCase()}</p>
+                </div>
+                <nav className="header_grid_item">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     )
-
-} 
+}
 
 export default Header;
