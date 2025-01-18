2520634ba7ffd701fa3ab78cb930a16c1f033d91
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-18 15:59:29 +0000

    added icons from font awesome to the h3 titles in project sections. Also adjusted styling. Need to add a tags around the icons and set up the href destinations. These projects are just placeholders until I create something worthwhile lol

--- my-first-react-project/src/projects.jsx
+++ my-first-react-project/src/projects.jsx
@@ -1,4 +1,5 @@
 import gsap from 'gsap';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Projects() {
 
@@ -15,7 +16,34 @@ function Projects() {
         "Folder Management"
     ];
 
-    const react_cv_desc = "poop";
+    const react_cv_desc = "This React project creates a responsive, interactive online portfolio and CV.\
+     Built with React, HTML, CSS, and JavaScript, it highlights skills and experiences while offering an engaging user experience with modern web design.";
+
+    const react_cv_skills = [
+        "React",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Responsive Design",
+        "Performance Optimization",
+        "UI/UX Design",
+        "Interactive Portfolio"
+    ];
+
+    const kankei_desc = "This project involved developing a modern & professional website.\
+     Designs and prototypes were created in Figma, with demonstrations built using HTML, CSS & JS.\
+     Final website implemented on WordPress for client flexibility."
+
+    const kankei_skills = [
+        "Web Developemnt",
+        "Client Collaboration",
+        "SEO Basics",
+        "Prototyping",
+        "Wireframes",
+        "UI / UX Design",
+        "Figma"
+
+    ];
 
 
 
@@ -24,47 +52,91 @@ function Projects() {
 
     return (
         <>
-            <div className="projects_grid">
+            <section className="projects_section">
+
+                <div className="projects_grid">
 
                 <div className = "project">
-                    <div className="trapezoid">
-                    </div>
-                    <div className="project_description">
-                        <h4>Noteworthy Project</h4>
-                        <h3>React CV</h3>
-                        <p>{react_cv_desc}</p>
-                    </div>
-                    <div className="project_extra_info">
-                        <div className="project_skills">
-                            {email_auto_skills.map((skill, index) => {
-                                return (
-                                    <div key={index} className="project_skill"># {skill}</div>
-                                )
-                            })}
+                        <div className="trapezoid">
+                        </div>
+                        <div className="project_description">
+                            <h4>Noteworthy Project</h4>
+
+                            <h3>KANKEI 
+                                <span className = "project_icons">
+                                    <i class="fa-brands fa-wordpress"></i>
+                                </span>
+                            </h3>
+
+                            <p>{kankei_desc}</p>
+                        </div>
+                        <div className="project_extra_info">
+                            <div className="project_skills">
+                                {kankei_skills.map((skill, index) => {
+                                    return (
+                                        <div key={index} className="project_skill"># {skill}</div>
+                                    )
+                                })}
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className = "project">
-                    <div className="trapezoid">
-                    </div>
-                    <div className="project_description">
-                        <h4>Noteworthy Project</h4>
-                        <h3>Email Automation</h3>
-                        <p>{email_auto_desc}</p>
-                    </div>
-                    <div className="project_extra_info">
-                        <div className="project_skills">
-                            {email_auto_skills.map((skill, index) => {
-                                return (
-                                    <div key={index} className="project_skill"># {skill}</div>
-                                )
-                            })}
+                    <div className = "project">
+                        <div className="trapezoid">
+                        </div>
+                        <div className="project_description">
+                            <h4>Noteworthy Project</h4>
+
+                            <h3>React CV 
+                                <span className = "project_icons">
+                                    <i class="fa-brands fa-github"></i>
+                                </span>
+                                <span className = "project_icons">
+                                    <i class="fa-solid fa-globe"></i>
+                                </span>
+                            </h3>
+                            
+                            <p>{react_cv_desc}</p>
+                        </div>
+                        <div className="project_extra_info">
+                            <div className="project_skills">
+                                {react_cv_skills.map((skill, index) => {
+                                    return (
+                                        <div key={index} className="project_skill"># {skill}</div>
+                                    )
+                                })}
+                            </div>
                         </div>
                     </div>
+
+                    <div className = "project">
+                        <div className="trapezoid">
+                        </div>
+                        <div className="project_description">
+                            <h4>Noteworthy Project</h4>
+
+                            <h3>Email Automation 
+                                <span className = "project_icons">
+                                    <i class="fa-brands fa-github"></i>
+                                </span>
+                            </h3>
+                            
+                            <p>{email_auto_desc}</p>
+                        </div>
+                        <div className="project_extra_info">
+                            <div className="project_skills">
+                                {email_auto_skills.map((skill, index) => {
+                                    return (
+                                        <div key={index} className="project_skill"># {skill}</div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
-            </div>  
+            </section>
         
         </>
     )
