34606321814da80778f43285724e2af7cc7311f5
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-26 18:17:42 +0000

    trying forsubmit.co - added action/method to from tag. corrected name attributes within inputs on form

--- my-first-react-project/src/contact.jsx
+++ my-first-react-project/src/contact.jsx
@@ -15,20 +15,19 @@ function Contact() {
                     <div className="contact_grid">
                         <div className = "contact_form_grid">
 
-                            <div className="contact_form_item">
-                                <label htmlFor="name">Your name</label>
-                                <input type="text" id="name" name="value" placeholder="Your name" />
-                            </div>
-
-                            <div className="contact_form_item">
-                                <label htmlFor="email">Your email</label>
-                                <input className="contact_form_email_input" type="email" id="email" name="value" placeholder="Your email" />
-                            </div>
-
-                            <label htmlFor="message">Message / Enquiry</label>
-                            <textarea className="contact_form_message" name="message" id="message" placeholder="Enter your message here"></textarea>
-
-                            <input className="contact_form_submit" type="submit" value="Send" />
+                            <form action="https://formsubmit.co/bigdavecoding@gmail.com" method="POST">
+                                <div className="contact_form_item">
+                                    <label htmlFor="name">Your name</label>
+                                    <input type="text" id="name" name="name" placeholder="Your name" />
+                                </div>
+                                <div className="contact_form_item">
+                                    <label htmlFor="email">Your email</label>
+                                    <input className="contact_form_email_input" type="email" id="email" name="email" placeholder="Your email" />
+                                </div>
+                                <label htmlFor="message">Message / Enquiry</label>
+                                <textarea className="contact_form_message" name="message" id="message" placeholder="Enter your message here"></textarea>
+                                <input className="contact_form_submit" type="submit" value="Send" />
+                            </form>
 
                         </div>
                         <div className = "contact_info_socials">
